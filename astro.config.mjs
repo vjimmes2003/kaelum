6f0538ab8a26113acdf95fadeaee33c66b38a9f3
vjimmes2003 <victorhugojimenezmeseguer@gmail.com>
2026-01-15 09:25:07 +0100

    fin del inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://vjimmes2003.github.io",
-  base: "/kaelum/",
+  base: "/",
   trailingSlash: "always",
 });
