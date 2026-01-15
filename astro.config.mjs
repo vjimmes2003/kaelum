47363f8059291c07c367e1822fe0f315436831cf
vjimmes2003 <victorhugojimenezmeseguer@gmail.com>
2026-01-15 08:06:31 +0100

    Add github pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://vjimmes2003.github.io",
+  base: "/kaelum",
+});
+
